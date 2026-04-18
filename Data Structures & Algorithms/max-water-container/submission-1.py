76940b5d299b4726368eff4ad2f8fddf0e9553f8
Calum Davies <calumdavies08@gmail.com>
2026-04-18 16:49:05 +0100

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+    
+        maxVol = 0 #vol = width * min(left height, right height)
+
+        while l < r:
+            currVol = (r-l) * min(heights[l], heights[r])
+            
+            #only increment/decrement the smaller bar
+            if heights[l] < heights[r]:
+                l += 1  
+            elif heights[l] > heights[r]:
+                r -= 1
+            else:
+                l += 1
+            if currVol > maxVol:
+                maxVol = currVol
+
+        return maxVol
+
+        #time O(n) space O(1) this one was a lot easier than 3Sum
+        # I should actually only be moving 1 pointer if heights are equal, works either way but ill make another submission
+
+
+            
+
+            
+            
+            
+
+        
+
+                
+
+        
