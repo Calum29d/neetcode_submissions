ccae44fb6a0eb86f0e95f060747b9fa6232ea3fc
Calum Davies <calumdavies08@gmail.com>
2026-06-12 18:01:35 +0100

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        #Valid Graph = connected, Acyclic and for n vertices there is n - 1 edges
+
+        #we know its not a valid graph is num of vertices - 1 does not equal num of edges
+        if n - 1 != len(edges):
+            return False
+        
+        #make adjList for undirected list
+        adjMap = defaultdict(list)
+        for node1, node2 in edges:
+            adjMap[node1].append(node2)
+            adjMap[node2].append(node1)
+
+        visit = set()
+
+        def dfs(node, prevNode):
+            #if cycle exists
+            if node in visit:
+                return False
+
+            #visit the node
+            visit.add(node)
+
+            for neighbor in adjMap[node]:
+                #dont go back to the prev node because then it would count as a cycle
+                if neighbor == prevNode:
+                    continue
+                if not dfs(neighbor, node): return False
+            
+            return True
+
+        #second condition to check if the graph is connected
+        return dfs(0,None) and len(visit) == n
+        
+                
+            
+
+
+
+        
+
+
