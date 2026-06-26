f40a8416b73fa0bd4d046a66ec69bf2cbd31c0da
Calum Davies <calumdavies08@gmail.com>
2026-06-26 17:54:43 +0100

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        def simpleRob(nums):
+
+            rob1, rob2 = 0, 0
+
+            for n in nums:
+                #decide whether its better to rob the current house or skip it
+                temp = max(n + rob1, rob2)
+                rob1 = rob2
+                rob2 = temp
+            
+            return rob2
+        
+        #skip the first house and the last house and return whatever way or robbing is greater
+        return nums[0] if len(nums) == 1 else max(simpleRob(nums[1:]), simpleRob(nums[:-1]))
+
+        
