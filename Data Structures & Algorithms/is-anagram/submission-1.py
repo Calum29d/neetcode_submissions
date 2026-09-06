079e7f2d5a40e1b607d731c4772927f682c0bfae
Calum Davies <calumdavies08@gmail.com>
2026-09-06 14:42:53 +0100

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False  
+        
+        sFreq = {}
+        tFreq = {}
+
+        for i in range(len(s)):
+            sFreq[s[i]] = sFreq.get(s[i], 0) + 1
+            tFreq[t[i]] = tFreq.get(t[i], 0) + 1
+        
+        return sFreq == tFreq
+
+        # O(n) time where n is the length of s or t (they are the same length)
+        # O(n + m) space 
