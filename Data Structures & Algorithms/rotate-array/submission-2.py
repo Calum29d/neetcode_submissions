14f2dfb2656042f6efce073a8f95917240a8007b
Calum Davies <calumdavies08@gmail.com>
2026-09-14 14:51:39 +0100

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        for i in range(k):
+            endNum = nums.pop()
+            nums.insert(0, endNum)
+
+        # O(n^2) time 
+        # O(1) space
+
+        
